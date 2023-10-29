a6f5e5d65983e0b2a73bf8058df3a9eb01bd399d
Alwaysgone <ffxiizocker@hotmail.com>
2023-10-29 19:22:26 +0100

    Added AutoswimController as new user interface to get and mutate state

---
+++ autoswim-runtime/src/main/java/io/autoswim/runtime/AutoswimController.java
@@ -0,0 +1,43 @@
+package io.autoswim.runtime;
+
+import java.time.Instant;
+import java.util.function.UnaryOperator;
+
+import org.automerge.ChangeHash;
+import org.automerge.Document;
+
+import io.autoswim.MessageIdGenerator;
+import io.autoswim.OwnEndpointProvider;
+import io.autoswim.messages.IncrementalSyncMessage;
+import io.autoswim.types.Endpoint;
+
+public class AutoswimController {
+	private final AutoswimRuntime runtime;
+	private final AutoswimStateHandler stateHandler;
+	private final MessageIdGenerator idGen;
+	private final Endpoint ownEndpoint;
+
+	public AutoswimController(AutoswimRuntime runtime,
+			AutoswimStateHandler stateHandler,
+			MessageIdGenerator idGen,
+			OwnEndpointProvider ownEndpointProvider) {
+				this.runtime = runtime;
+				this.stateHandler = stateHandler;
+				this.idGen = idGen;
+				this.ownEndpoint = ownEndpointProvider.getOwnEndpoint();
+	}
+	
+	public Document getCurrentState() {
+		return stateHandler.getCurrentState();
+	}
+	
+	public void updateState(UnaryOperator<Document> update) {
+		byte[] changeSet = stateHandler.updateState(update);
+		runtime.scheduleMessage(IncrementalSyncMessage.builder()
+				.withChangeSet(changeSet)
+				.withCreatedAt(Instant.now())
+				.withId(idGen.generateId())
+				.withSender(ownEndpoint)
+				.build());
+	}
+}
