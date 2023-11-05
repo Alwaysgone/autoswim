591eec869ab8fbbf57a3b7160fadc2e69c5c25c0
Alwaysgone <ffxiizocker@hotmail.com>
2023-11-05 16:35:38 +0100

    Added cdi module which only requires an AutoswimConfig to be bound to work

---
+++ autoswim-cdi/src/main/java/io/autoswim/cdi/AutoswimProducer.java
@@ -0,0 +1,83 @@
+package io.autoswim.cdi;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import io.autoswim.AutoswimException;
+import io.autoswim.AutoswimRequestSyncScheduler;
+import io.autoswim.MessageIdGenerator;
+import io.autoswim.OwnEndpointProvider;
+import io.autoswim.UuidMessageIdGenerator;
+import io.autoswim.runtime.AutoswimConfig;
+import io.autoswim.runtime.AutoswimRuntime;
+import io.autoswim.runtime.AutoswimStateHandler;
+import io.autoswim.runtime.AutoswimStateInitializer;
+import io.autoswim.runtime.EmptyAutoswimStateInitializer;
+import io.autoswim.runtime.ExecutorAutoswimRequestSyncScheduler;
+import io.autoswim.swim.network.AutoswimNetwork;
+import io.autoswim.swim.scalecube.network.ScaleCubeAutoswimNetwork;
+import io.autoswim.types.Endpoint;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+
+@ApplicationScoped
+public class AutoswimProducer {
+	
+	@ApplicationScoped
+	@Produces
+	public AutoswimRuntime produceAutoswimRuntime(AutoswimConfig config,
+			AutoswimNetwork network,
+			AutoswimStateHandler stateHandler,
+			OwnEndpointProvider ownEndpointProvider,
+			MessageIdGenerator messageIdGenerator,
+			AutoswimRequestSyncScheduler requestSyncScheduler) {
+		return new AutoswimRuntime(config, network, stateHandler, ownEndpointProvider, messageIdGenerator, requestSyncScheduler);
+	}
+	
+	@ApplicationScoped
+	@Produces
+	public AutoswimNetwork produceAutoswimNetwork(AutoswimConfig config) {
+		return new ScaleCubeAutoswimNetwork(config.getNetworkConfig());
+	}
+	
+	@ApplicationScoped
+	@Produces
+	public AutoswimStateHandler produceAutoswimStateHandler(AutoswimConfig config, AutoswimStateInitializer stateInitializer) {
+		return new AutoswimStateHandler(config.getAutoswimWorkingDir(), stateInitializer);
+	}
+	
+	@ApplicationScoped
+	@Produces
+	public AutoswimStateInitializer produceAutoswimStateInitializer() {
+		return new EmptyAutoswimStateInitializer();
+	}
+	
+	@ApplicationScoped
+	@Produces
+	public MessageIdGenerator produceMessageIdGenerator() {
+		return new UuidMessageIdGenerator();
+	}
+	
+	@ApplicationScoped
+	@Produces
+	public AutoswimRequestSyncScheduler produceAutoswimRequestSyncScheduler(AutoswimNetwork swimNetwork,
+			AutoswimStateHandler stateHandler,
+			OwnEndpointProvider ownEndpointProvider,
+			MessageIdGenerator messageIdGenerator) {
+		return new ExecutorAutoswimRequestSyncScheduler(swimNetwork, stateHandler, ownEndpointProvider, messageIdGenerator);
+	}
+	
+	@ApplicationScoped
+	@Produces
+	public OwnEndpointProvider produceOwnEndpointProvider(AutoswimConfig config) {
+		try {
+			String hostname = InetAddress.getLocalHost().getHostName();
+			return new OwnEndpointProvider(Endpoint.builder()
+					.withHostname(hostname)
+					.withPort(config.getNetworkConfig().getSwimPort())
+					.build());
+		} catch (UnknownHostException e) {
+			throw new AutoswimException("Could not get hostname", e);
+		}
+	}
+}
--- autoswim-runtime/src/main/java/io/autoswim/runtime/AutoswimConfig.java
+++ autoswim-runtime/src/main/java/io/autoswim/runtime/AutoswimConfig.java
@@ -1,14 +1,38 @@
 package io.autoswim.runtime;
 
+import java.nio.file.Path;
+
+import io.autoswim.swim.network.AutoswimNetworkConfig;
+
 public class AutoswimConfig {
 	private final boolean registerDefaultMessageHandler;
+	private final Path autoswimWorkingDir;
+	private final AutoswimNetworkConfig networkConfig;
 
 	private AutoswimConfig(Builder builder) {
 		this.registerDefaultMessageHandler = builder.registerDefaultMessageHandler;
+		this.autoswimWorkingDir = builder.autoswimWorkingDir;
+		this.networkConfig = builder.networkConfig;
 	}
-	
+
+	/**
+	 * If set to false the default Autoswim message handler is not registered, meaning
+	 * all updates need to be handled by a user provided message handler.
+	 * This is useful if there is either a bug in the default implementation or
+	 * the user needs to apply additional logic during sync operations.
+	 * @param registerDefaultMessageHandler whether to enable registering the default message handler or not
+	 * @return the builder instance
+	 */
 	public boolean isRegisterDefaultMessageHandler() {
 		return registerDefaultMessageHandler;
+	}
+	
+	public Path getAutoswimWorkingDir() {
+		return autoswimWorkingDir;
+	}
+	
+	public AutoswimNetworkConfig getNetworkConfig() {
+		return networkConfig;
 	}
 
 	public static Builder builder() {
@@ -20,25 +44,31 @@ public class AutoswimConfig {
 	}
 
 	public static final class Builder {
-		private boolean registerDefaultMessageHandler = true;
+		private boolean registerDefaultMessageHandler;
+		private Path autoswimWorkingDir;
+		private AutoswimNetworkConfig networkConfig;
 
 		private Builder() {
 		}
 
 		private Builder(AutoswimConfig autoswimConfig) {
 			this.registerDefaultMessageHandler = autoswimConfig.registerDefaultMessageHandler;
+			this.autoswimWorkingDir = autoswimConfig.autoswimWorkingDir;
+			this.networkConfig = autoswimConfig.networkConfig;
 		}
 
-		/**
-		 * If set to false the default Autoswim message handler is not registered, meaning
-		 * all updates need to be handled by a user provided message handler.
-		 * This is useful if there is either a bug in the default implementation or
-		 * the user needs to apply additional logic during sync operations.
-		 * @param registerDefaultMessageHandler whether to enable registering the default message handler or not
-		 * @return the builder instance
-		 */
 		public Builder withRegisterDefaultMessageHandler(boolean registerDefaultMessageHandler) {
 			this.registerDefaultMessageHandler = registerDefaultMessageHandler;
+			return this;
+		}
+
+		public Builder withAutoswimWorkingDir(Path autoswimWorkingDir) {
+			this.autoswimWorkingDir = autoswimWorkingDir;
+			return this;
+		}
+
+		public Builder withNetworkConfig(AutoswimNetworkConfig networkConfig) {
+			this.networkConfig = networkConfig;
 			return this;
 		}
 
